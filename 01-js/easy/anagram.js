03789b44c457ec30071373f31dbf4bce7d7c4cb3
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2023-12-20 07:55:46 +0000

    added solutions to week 1 assignments

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -3,37 +3,27 @@
   What's Anagram?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
+function sort(str){
+  let array=str.split("");
+  array=array.sort();
+  let sortedStr=array.join("");
+  return sortedStr;
+
+}
+
 
 function isAnagram(str1, str2) {
 
-  let NO_OF_CHARS = 256;
-  if (str1.length != str2.length)
-      return false;
+  let strA=str1.toLower();
+  let strB=str2.toLower();
 
-  let count1 = new Array(NO_OF_CHARS);
-  let count2 = new Array(NO_OF_CHARS);
-  for(let i = 0; i < NO_OF_CHARS; i++)
-  {
-      count1[i] = 0;
-      count2[i] = 0;
+  if (sort(strA)==sort(strB)){
+    return true;
   }
-   
-  let i;
-
-
-  for (i = 0; i < str1.length && i < str2.length;
-       i++) {
-      count1[str1[i].charCodeAt(0)]++;
-      count2[str1[i].charCodeAt(0)]++;
+  else{
+    return false;
   }
 
-
-  for (i = 0; i < NO_OF_CHARS; i++)
-      if (count1[i] != count2[i])
-          return false;
-
-  return true;
-
-}
+}  
 
 module.exports = isAnagram;
